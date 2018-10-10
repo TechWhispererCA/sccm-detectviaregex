db87da2536c81f2dad75fce3f82da0d9681ef1d4
Kyle Erickson <kyle@techwhisperer.ca>
2018-10-09 20:47:50 -0600

    Update DetectViaRegistryRegEx.ps1
    Updated code to use a function that will try a version comparision first, then integer, then fall back to string if required.  Original method was using a string only, and would try to compare based on the matching number of digits in the major version.

--- DetectViaRegistryRegEx.ps1
+++ DetectViaRegistryRegEx.ps1
@@ -1,8 +1,53 @@
 ﻿Try {
-    [string]$appNameRegEx = '^Cisco AnyConnect'
-    [string]$appVersion = '3.1.10010'
-    [bool]$matchFound = $false
-    [string[]]$regKeyBranches = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    [string]$appNameRegEx = '^Application Name'
+    [string]$appVersion = '1.0.0.0'
+    [string]$appVendorRegEx = 'Vendor'
+    
+    # Nothing should need to be modified below this line
+
+    function Compare-Versions {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory=$true)]
+            [ValidateNotNullorEmpty()]
+            [string]$DeploymentVersion,
+            [Parameter(Mandatory=$true)]
+            [ValidateNotNullorEmpty()]
+            [string]$DetectedVersion
+        )
+        Try {
+            # Attempting to use the [version] type
+            [version]$vDeploymentVersion = $DeploymentVersion
+            [version]$vDetectedVersion = $DetectedVersion
+            if ($vDetectedVersion -ge $vDeploymentVersion) {
+                Return $true
+            } else {
+                Return $false
+            }
+        } Catch {
+            if (($DetectedVersion -notmatch '\D') -and ($DeploymentVersion -notmatch '\D')) {
+                # Both versions contain only numbers
+                Try {
+                    [int32]$intDetectedVersion = $DetectedVersion
+                    [int32]$intDeploymentVersion = $DeploymentVersion
+                    if ($intDetectedVersion -ge $intDeploymentVersion) {
+                        Return $true
+                    } else {
+                        Return $false
+                    }
+                } Catch {
+                    # Failing back to comparing as strings
+                }
+            }     
+            if ($DetectedVersion -ge $DeploymentVersion) {
+                Return $true
+            } else {
+                Return $false
+            }
+        }
+    }
+    
+    [string[]]$regKeyBranches = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
     :Search foreach ($regKeyBranch in $regKeyBranches) {
         If (Test-Path -LiteralPath $regKeyBranch -ErrorAction 'SilentlyContinue') {
             [psobject[]]$UninstallKeys = Get-ChildItem -Path $regKeyBranch -Recurse
@@ -10,48 +55,23 @@
                 $regKey = ($UninstallKey.PSChildName)
                 Try {$regKeyProperties = Get-ItemProperty -LiteralPath "$regKeyBranch\$regKey" -ErrorAction Stop}
                 Catch {Continue}
-                if (($regKeyProperties.DisplayName) -match $appNameRegEx) {
-                    switch -Regex ($appVersion) {
-                        '^\d\..*' {
-                            if (($regKeyProperties.DisplayVersion -match '^\d\..*') -and (($regKeyProperties.DisplayVersion) -ge $appVersion)) {
-                                [bool]$matchFound = $true
-                                Break
-                            } elseif (($regKeyProperties.DisplayVersion) -match '^\d{2,}\..*') {
-                                [bool]$matchFound = $true
-                                Break
-                            }
-                        }
-                        '^\d{2}\..*' {
-                            if (($regKeyProperties.DisplayVersion -match '^\d{2}\..*') -and (($regKeyProperties.DisplayVersion) -ge $appVersion)) {
-                                [bool]$matchFound = $true
-                                Break
-                            } elseif (($regKeyProperties.DisplayVersion) -match '^\d{3,}\..*') {
-                                [bool]$matchFound = $true
-                                Break
-                            }
-                        }
-                        '^\d{3}\..*' {
-                            if (($regKeyProperties.DisplayVersion -match '^\d{3}\..*') -and (($regKeyProperties.DisplayVersion) -ge $appVersion)) {
-                                [bool]$matchFound = $true
-                                Break
-                            } elseif (($regKeyProperties.DisplayVersion) -match '^\d{4,}\..*') {
-                                [bool]$matchFound = $true
-                                Break
-                            }
-                        }
-                        default {Throw 'Check version number.'}
+                if ((($regKeyProperties.DisplayName) -match $appNameRegEx) -and (($regKeyProperties.Publisher) -match $appVendorRegEx)) {
+                    Try {
+                        if (($appVersion) -and (($regKeyProperties).DisplayVersion) -and (Compare-Versions -DeploymentVersion $appVersion -DetectedVersion (($regKeyProperties).DisplayVersion))) {
+                            Write-Output ($regKeyProperties.DisplayName)
+                            Write-Output ($regKeyProperties.DisplayVersion)
+                            Break Search
+                        }           
+                    } Catch {
+                        Continue
                     }
-
-                    if ($matchFound) {
-                        Write-Output ($regKeyProperties.DisplayName)
-                        Write-Output ($regKeyProperties.DisplayVersion)
-                        Break Search
-                    }             
                 }
             }
         }
     }
+
     Exit 0
+
 } Catch {
     Exit 60001
 }
